0b3307c1da799a99644671595d30f3fecff3ee51
Rob1in <73953872+Rob1in@users.noreply.github.com>
2023-03-31 14:49:20 -0400

    first commit

---
+++ example/decoder/go.mod
@@ -0,0 +1,7 @@
+module main
+
+go 1.19
+
+require github.com/viam-labs/go-libjpeg v0.3.1
+
+replace github.com/viam-labs/go-libjpeg => /Users/robinin/fork/test/go-libjpeg
--- example/decoder/main.go
+++ example/decoder/main.go
@@ -1,41 +1,49 @@
 package main
 
 import (
-	"flag"
+	"bytes"
+	"fmt"
 	"image"
+	"image/png"
 	"log"
 	"os"
+	"path/filepath"
 
 	"github.com/viam-labs/go-libjpeg/jpeg"
 )
 
-func main() {
-	flag.Parse()
-	file := flag.Arg(0)
-
-	io, err := os.Open(file)
+func pngToImage(path string) image.Image {
+	openBytes, err := os.ReadFile(path)
 	if err != nil {
-		log.Fatalln("Can't open file: ", file)
+		panic(err.Error())
 	}
+	img, err := png.Decode(bytes.NewReader(openBytes))
+	if err != nil {
+		panic(err.Error())
+	}
+	return img
+}
 
-	img, err := jpeg.Decode(io, &jpeg.DecoderOptions{})
+func main() {
+	img := pngToImage("data/landscape.png")
 	if img == nil {
 		log.Fatalln("Got nil")
 	}
+	img.ColorModel()
+	//bf := new(bytes.Buffer)
+	f, err := os.Create(filepath.Clean("data/encoded_ouais.jpeg"))
+	err = jpeg.Encode(f, img, &jpeg.EncoderOptions{Quality: 2})
 	if err != nil {
 		log.Fatalf("Got Error: %v", err)
 	}
-
-	//
-	// write your code here ...
-	//
-
-	switch img.(type) {
-	case *image.YCbCr:
-		log.Println("decoded YCbCr")
-	case *image.Gray:
-		log.Println("decoded Gray")
-	default:
-		log.Println("unknown format")
-	}
+	fmt.Println(img.Bounds().Dx())
+	fmt.Println(img.Bounds().Dy())
+	//switch img.(type) {
+	//case *image.YCbCr:
+	//	log.Println("decoded YCbCr")
+	//case *image.Gray:
+	//	log.Println("decoded Gray")
+	//default:
+	//	log.Println("unknown format")
+	//}
 }
