3a4fa81ee60af0681fb5cdd7d8c0beaf00c9e731
Rob1in <73953872+Rob1in@users.noreply.github.com>
2023-04-12 14:52:30 -0400

    first commit

--- jpeg/jpeg.go
+++ jpeg/jpeg.go
@@ -27,53 +27,111 @@ static J_COLOR_SPACE getJCS_EXT_RGBA(void) {
 SymbolsTable symbols_table;
 
 
-void get_function_ptr(void **function_ptr, const char *symbol_name) {
-  // Clear any previous errors
-  dlerror();
+//void get_function_ptr(void **function_ptr, const char *symbol_name) {
+//  // Clear any previous errors
+//  dlerror();
+//
+//  // Use dlsym to get the function pointer for the specified symbol name
+//  void *function = dlsym(RTLD_DEFAULT, symbol_name);
+//
+//  // Check for errors
+//  char *error = dlerror();
+//  if (error != NULL) {
+//      fprintf(stderr, "Error while looking up symbol %s: %s\n", symbol_name, error);
+//      return;
+//  }
+//
+//  // Assign the function pointer to the specified function pointer variable
+//  *function_ptr = function;
+//}
+//
+//void populate_symbols_table(SymbolsTable *symbols){
+//	//symbols for compression
+//	get_function_ptr((void**)&symbols->jpeg_start_compress_ptr, "jpeg_start_compress");
+//	get_function_ptr((void**)&symbols->jpeg_CreateCompress_ptr, "jpeg_CreateCompress");
+//	get_function_ptr((void**)&symbols->jpeg_std_error_ptr, "jpeg_std_error");
+//	get_function_ptr((void**)&symbols->jpeg_destroy_compress_ptr, "jpeg_destroy_compress");
+//	get_function_ptr((void**)&symbols->jpeg_set_defaults_ptr, "jpeg_set_defaults");
+//	get_function_ptr((void**)&symbols->jpeg_set_quality_ptr, "jpeg_set_quality");
+//	get_function_ptr((void**)&symbols->jpeg_simple_progression_ptr, "jpeg_simple_progression");
+//	get_function_ptr((void**)&symbols->jpeg_finish_compress_ptr, "jpeg_finish_compress");
+//	get_function_ptr((void**)&symbols->jpeg_write_raw_data_ptr, "jpeg_write_raw_data");
+//	get_function_ptr((void**)&symbols->jpeg_write_scanlines_ptr, "jpeg_write_scanlines");
+//
+//	//symbols for decompression
+//	get_function_ptr((void**)&symbols->jpeg_CreateDecompress_ptr, "jpeg_CreateDecompress");
+//	get_function_ptr((void**)&symbols->jpeg_resync_to_restart_ptr, "jpeg_resync_to_restart");
+//	get_function_ptr((void**)&symbols->jpeg_destroy_decompress_ptr, "jpeg_destroy_decompress");
+//	get_function_ptr((void**)&symbols->jpeg_read_header_ptr, "jpeg_read_header");
+//	get_function_ptr((void**)&symbols->jpeg_start_decompress_ptr, "jpeg_start_decompress");
+//	get_function_ptr((void**)&symbols->jpeg_read_raw_data_ptr, "jpeg_read_raw_data");
+//	get_function_ptr((void**)&symbols->jpeg_finish_decompress_ptr, "jpeg_finish_decompress");
+//	get_function_ptr((void**)&symbols->jpeg_calc_output_dimensions_ptr, "jpeg_calc_output_dimensions");
+//	get_function_ptr((void**)&symbols->jpeg_read_scanlines_ptr, "jpeg_read_scanlines");
+//
+//}
+//
+void get_function_ptr_from_handle(void *lib_handle, void **function_ptr, const char *symbol_name) {
+ // Clear any previous errors
+ dlerror();
 
-  // Use dlsym to get the function pointer for the specified symbol name
-  void *function = dlsym(RTLD_DEFAULT, symbol_name);
+ // Use dlsym to get the function pointer for the specified symbol name
+ void *function = dlsym(lib_handle, symbol_name);
 
-  // Check for errors
-  char *error = dlerror();
-  if (error != NULL) {
-      fprintf(stderr, "Error while looking up symbol %s: %s\n", symbol_name, error);
-      return;
-  }
+ // Check for errors
+ char *error = dlerror();
+ if (error != NULL) {
+     fprintf(stderr, "Error while looking up symbol %s: %s\n", symbol_name, error);
+     return;
+ }
 
-  // Assign the function pointer to the specified function pointer variable
-  *function_ptr = function;
+ // Assign the function pointer to the specified function pointer variable
+ *function_ptr = function;
 }
 
-void populate_symbols_table(SymbolsTable *symbols){
+void populate_symbols_table_from_handle(void *lib_handle, SymbolsTable *symbols){
 	//symbols for compression
-	get_function_ptr((void**)&symbols->jpeg_start_compress_ptr, "jpeg_start_compress");
-	get_function_ptr((void**)&symbols->jpeg_CreateCompress_ptr, "jpeg_CreateCompress");
-	get_function_ptr((void**)&symbols->jpeg_std_error_ptr, "jpeg_std_error");
-	get_function_ptr((void**)&symbols->jpeg_destroy_compress_ptr, "jpeg_destroy_compress");
-	get_function_ptr((void**)&symbols->jpeg_set_defaults_ptr, "jpeg_set_defaults");
-	get_function_ptr((void**)&symbols->jpeg_set_quality_ptr, "jpeg_set_quality");
-	get_function_ptr((void**)&symbols->jpeg_simple_progression_ptr, "jpeg_simple_progression");
-	get_function_ptr((void**)&symbols->jpeg_finish_compress_ptr, "jpeg_finish_compress");
-	get_function_ptr((void**)&symbols->jpeg_write_raw_data_ptr, "jpeg_write_raw_data");
-	get_function_ptr((void**)&symbols->jpeg_write_scanlines_ptr, "jpeg_write_scanlines");
+	get_function_ptr_from_handle(lib_handle, (void**)&symbols->jpeg_start_compress_ptr, "jpeg_start_compress");
+	get_function_ptr_from_handle(lib_handle, (void**)&symbols->jpeg_CreateCompress_ptr, "jpeg_CreateCompress");
+	get_function_ptr_from_handle(lib_handle, (void**)&symbols->jpeg_std_error_ptr, "jpeg_std_error");
+	get_function_ptr_from_handle(lib_handle, (void**)&symbols->jpeg_destroy_compress_ptr, "jpeg_destroy_compress");
+	get_function_ptr_from_handle(lib_handle, (void**)&symbols->jpeg_set_defaults_ptr, "jpeg_set_defaults");
+	get_function_ptr_from_handle(lib_handle, (void**)&symbols->jpeg_set_quality_ptr, "jpeg_set_quality");
+	get_function_ptr_from_handle(lib_handle, (void**)&symbols->jpeg_simple_progression_ptr, "jpeg_simple_progression");
+	get_function_ptr_from_handle(lib_handle, (void**)&symbols->jpeg_finish_compress_ptr, "jpeg_finish_compress");
+	get_function_ptr_from_handle(lib_handle, (void**)&symbols->jpeg_write_raw_data_ptr, "jpeg_write_raw_data");
+	get_function_ptr_from_handle(lib_handle, (void**)&symbols->jpeg_write_scanlines_ptr, "jpeg_write_scanlines");
 
 	//symbols for decompression
-	get_function_ptr((void**)&symbols->jpeg_CreateDecompress_ptr, "jpeg_CreateDecompress");
-	get_function_ptr((void**)&symbols->jpeg_resync_to_restart_ptr, "jpeg_resync_to_restart");
-	get_function_ptr((void**)&symbols->jpeg_destroy_decompress_ptr, "jpeg_destroy_decompress");
-	get_function_ptr((void**)&symbols->jpeg_read_header_ptr, "jpeg_read_header");
-	get_function_ptr((void**)&symbols->jpeg_start_decompress_ptr, "jpeg_start_decompress");
-	get_function_ptr((void**)&symbols->jpeg_read_raw_data_ptr, "jpeg_read_raw_data");
-	get_function_ptr((void**)&symbols->jpeg_finish_decompress_ptr, "jpeg_finish_decompress");
-	get_function_ptr((void**)&symbols->jpeg_calc_output_dimensions_ptr, "jpeg_calc_output_dimensions");
-	get_function_ptr((void**)&symbols->jpeg_read_scanlines_ptr, "jpeg_read_scanlines");
+	get_function_ptr_from_handle(lib_handle, (void**)&symbols->jpeg_CreateDecompress_ptr, "jpeg_CreateDecompress");
+	get_function_ptr_from_handle(lib_handle, (void**)&symbols->jpeg_resync_to_restart_ptr, "jpeg_resync_to_restart");
+	get_function_ptr_from_handle(lib_handle, (void**)&symbols->jpeg_destroy_decompress_ptr, "jpeg_destroy_decompress");
+	get_function_ptr_from_handle(lib_handle, (void**)&symbols->jpeg_read_header_ptr, "jpeg_read_header");
+	get_function_ptr_from_handle(lib_handle, (void**)&symbols->jpeg_start_decompress_ptr, "jpeg_start_decompress");
+	get_function_ptr_from_handle(lib_handle, (void**)&symbols->jpeg_read_raw_data_ptr, "jpeg_read_raw_data");
+	get_function_ptr_from_handle(lib_handle, (void**)&symbols->jpeg_finish_decompress_ptr, "jpeg_finish_decompress");
+	get_function_ptr_from_handle(lib_handle, (void**)&symbols->jpeg_calc_output_dimensions_ptr, "jpeg_calc_output_dimensions");
+	get_function_ptr_from_handle(lib_handle, (void**)&symbols->jpeg_read_scanlines_ptr, "jpeg_read_scanlines");
 
 }
 
 
 void init(){
-	populate_symbols_table(&symbols_table);
+	void *lib_handle;
+	lib_handle = dlopen("/opt/homebrew/lib/libjpeg.8.2.2.dylib", RTLD_NOW);
+	if (!lib_handle) {
+		fprintf(stderr, "%s\n", dlerror());
+		exit(1);
+	}
+
+	//dlerror();
+	//void *function = dlsym(lib_handle, "jpeg_CreateDecompress");
+	//char *error = dlerror();
+	//if (error != NULL) {
+	//	  fprintf(stderr, "Error while looking up symbol %s: %s\n", "jpeg_CreateDecompress", error);
+	//	  return;
+	//  }
+	populate_symbols_table_from_handle(lib_handle, &symbols_table);
 }
 
 */
