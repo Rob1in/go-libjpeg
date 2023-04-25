0dfa892af9713d937d3d051edc8071270e534eea
James Otting <james@viam.com>
2023-04-25 12:07:15 -0500

    Update static libraries

--- jpeg/jpeg_static.go
+++ jpeg/jpeg_static.go
@@ -1,12 +1,12 @@
 //go:build !dynamic
-// +build !dynamic
 
 package jpeg
 
 /*
 #cgo CFLAGS: -I${SRCDIR}/include
-#cgo linux, arm64 LDFLAGS: -L${SRCDIR}/lib/libjpeg_linux_arm64.a
-#cgo darwin, arm64 LDFLAGS: -${SRCDIR}/lib/libjpeg_darwin_arm64.a
-
+#cgo linux,amd64 LDFLAGS: ${SRCDIR}/lib/libjpeg_linux_amd64.a
+#cgo linux,arm64 LDFLAGS: ${SRCDIR}/lib/libjpeg_linux_arm64.a
+#cgo darwin,amd64 LDFLAGS: ${SRCDIR}/lib/libjpeg_darwin_amd64.a
+#cgo darwin,arm64 LDFLAGS: ${SRCDIR}/lib/libjpeg_darwin_arm64.a
 */
-//import "C"
+import "C"
