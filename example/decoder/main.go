1d2b111ea2f8926b45f36dd40bdafbcb92994008
Rob1in <73953872+Rob1in@users.noreply.github.com>
2023-05-03 12:09:55 -0400

    Refactor test Revert the changes on the decoder test

--- example/decoder/main.go
+++ example/decoder/main.go
@@ -1,41 +1,41 @@
 package main
 
 import (
-	"bytes"
-	"fmt"
-	"github.com/viam-labs/go-libjpeg/jpeg"
+	"flag"
 	"image"
-	"image/png"
 	"log"
 	"os"
-	"path/filepath"
+
+	"github.com/viam-labs/go-libjpeg/jpeg"
 )
 
-func pngToImage(path string) image.Image {
-	openBytes, err := os.ReadFile(path)
-	if err != nil {
-		panic(err.Error())
-	}
-	img, err := png.Decode(bytes.NewReader(openBytes))
-	if err != nil {
-		panic(err.Error())
-	}
-	return img
-}
-
 func main() {
-	img := pngToImage("./data/landscape.png")
-	fmt.Println("tout va bien")
+	flag.Parse()
+	file := flag.Arg(0)
+
+	io, err := os.Open(file)
+	if err != nil {
+		log.Fatalln("Can't open file: ", file)
+	}
+
+	img, err := jpeg.Decode(io, &jpeg.DecoderOptions{})
 	if img == nil {
 		log.Fatalln("Got nil")
 	}
-	img.ColorModel()
-	//bf := new(bytes.Buffer)
-	f, err := os.Create(filepath.Clean("./data/encoded_normal2.jpeg"))
-	err = jpeg.Encode(f, img, &jpeg.EncoderOptions{Quality: 2})
 	if err != nil {
 		log.Fatalf("Got Error: %v", err)
 	}
-	fmt.Println(img.Bounds().Dx())
-	fmt.Println(img.Bounds().Dy())
+
+	//
+	// write your code here ...
+	//
+
+	switch img.(type) {
+	case *image.YCbCr:
+		log.Println("decoded YCbCr")
+	case *image.Gray:
+		log.Println("decoded Gray")
+	default:
+		log.Println("unknown format")
+	}
 }
